e1bc770c3187e3f197f8c60dfbcdc454703d6ddd
Morten Braten <mortenbra@users.noreply.github.com>
2015-07-20 21:53:00 +0200

    Install scripts now reference folders with forward slash

--- setup/install_amazon.sql
+++ setup/install_amazon.sql
@@ -4,13 +4,13 @@ set scan off;
 
 prompt Creating AMAZON package specifications
 
-@..\ora\amazon_aws_auth_pkg.pks
-@..\ora\amazon_aws_s3_pkg.pks
+@../ora/amazon_aws_auth_pkg.pks
+@../ora/amazon_aws_s3_pkg.pks
 
 prompt Creating AMAZON package bodies
 
-@..\ora\amazon_aws_auth_pkg.pkb
-@..\ora\amazon_aws_s3_pkg.pkb
+@../ora/amazon_aws_auth_pkg.pkb
+@../ora/amazon_aws_s3_pkg.pkb
 
 
 prompt Done!
